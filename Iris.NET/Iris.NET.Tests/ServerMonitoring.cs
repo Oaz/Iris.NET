b10b98cb2dca967254e77d466a19f178d9bf11ee
Oaz <github@azeau.com>
2017-09-09 18:11:43 +0200

    added some unit test examples

---
+++ Iris.NET/Iris.NET.Tests/ServerMonitoring.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using Iris.NET.Server;
+using System.Threading;
+using System.Net;
+using System.Reflection;
+
+namespace Iris.NET.Tests
+{
+	[TestFixture]
+    public class ServerMonitoring
+    {
+        IrisServer _theServer;
+
+		[SetUp]
+        public void Initialize()
+        {
+            _theServer = new IrisServer();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if(_theServer.IsRunning)
+                _theServer.Stop();
+        }
+
+		[Test]
+        public void ServerProperties()
+        {
+            Assert.AreEqual(false, _theServer.IsRunning);
+            Assert.AreEqual(null, _theServer.Port);
+            Assert.AreEqual(null, _theServer.Address);
+            _theServer.Start(56666);
+            Assert.AreEqual(true, _theServer.IsRunning);
+            Assert.AreEqual(56666, _theServer.Port);
+            Assert.AreEqual(new IPAddress(0), _theServer.Address);
+            _theServer.Stop();
+            Assert.AreEqual(false, _theServer.IsRunning);
+            Assert.AreEqual(null, _theServer.Port);
+            Assert.AreEqual(null, _theServer.Address);
+        }
+
+		[Test]
+        public void ServerIdAreUnique()
+        {
+            var otherServer = new IrisServer();
+            Assert.AreNotEqual(otherServer.Id, _theServer.Id);
+        }
+
+		[Test]
+        public void ServerStartCanBeMonitored()
+        {
+            var monitorStart = new AutoResetEvent(false);
+            _theServer.OnStarted += () => monitorStart.Set();
+            _theServer.Start(56666);
+            Assert.IsTrue(monitorStart.WaitOne(1000), "Server was not started");
+        }
+
+		[Test]
+        public void ServerStopCanBeMonitored()
+        {
+            var monitorStop = new AutoResetEvent(false);
+            _theServer.OnStopped += () => monitorStop.Set();
+            _theServer.Start(56666);
+            _theServer.Stop();
+            Assert.IsTrue(monitorStop.WaitOne(1000), "Server was not stopped");
+        }
+
+		[Test]
+        public void ServerFailureCanBeMonitored()
+        {
+            var monitorFailure = new AutoResetEvent(false);
+            _theServer.OnServerException += ex =>
+            {
+                Assert.IsNotNull(ex.Message);
+                monitorFailure.Set();
+            };
+            _theServer.Start(56666);
+            SimulateFailureByClosingTheServerTcpListener();
+            Assert.IsTrue(monitorFailure.WaitOne(2000), "Server failure was not detected");
+        }
+
+        private void SimulateFailureByClosingTheServerTcpListener()
+        {
+            var listener = (System.Net.Sockets.TcpListener) typeof(IrisServer).GetField(
+                "_serverSocket",
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField
+            ).GetValue(_theServer);
+            listener.Stop();
+        }
+    }
+}
---
+++ Iris.NET/Iris.NET.Tests/SimplePublicationSubscription.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using NUnit.Framework;
+using Iris.NET.Server;
+using System.Threading;
+using System.Threading.Tasks;
+using Iris.NET.Client;
+
+namespace Iris.NET.Tests
+{
+	[TestFixture]
+    public class SimplePublicationSubscription
+    {
+		[Test]
+        public async Task BetweenLocalNodes()
+        {
+            await PubSub<IrisLocalNode, IrisServerConfig, IrisLocalNode, IrisServerConfig>(
+                _theServer.GetServerConfig(),
+                _theServer.GetServerConfig());
+        }
+
+		[Test]
+        public async Task BetweenClientNodes()
+        {
+            await PubSub<IrisClientNode, IrisClientConfig, IrisClientNode, IrisClientConfig>(
+                _theClientConfig,
+                _theClientConfig);
+        }
+		[Test]
+        public async Task FromClientToLocalNode()
+        {
+            await PubSub<IrisClientNode, IrisClientConfig, IrisLocalNode, IrisServerConfig>(
+                _theClientConfig,
+                _theServer.GetServerConfig());
+        }
+
+        [Test]
+        public async Task FromLocalToClientNode()
+        {
+            await PubSub<IrisLocalNode, IrisServerConfig, IrisClientNode, IrisClientConfig>(
+                _theServer.GetServerConfig(),
+                _theClientConfig);
+        }
+
+        public async Task PubSub<PUBNODE, PUBCONF,SUBNODE, SUBCONF>(PUBCONF pubconf, SUBCONF subconf)
+            where SUBNODE : AbstractIrisNode<SUBCONF>, new()
+            where SUBCONF : IrisBaseConfig
+            where PUBNODE : AbstractIrisNode<PUBCONF>, new()
+            where PUBCONF : IrisBaseConfig
+        {
+            var received = new AutoResetEvent(false);
+            var subnode = new SUBNODE();
+            subnode.Connect(subconf);
+            using (var subscription = await subnode.Subscribe("TheFooChannel", (content, hook) =>
+            {
+                Assert.AreEqual("Foo ==> Bar", content);
+                received.Set();
+            }))
+            {
+                Assert.IsNotNull(subscription);
+                var pubnode = new PUBNODE();
+                pubnode.Connect(pubconf);
+                var publicationIsOk = await pubnode.Publish("TheFooChannel", "Foo ==> Bar");
+                Assert.IsTrue(publicationIsOk);
+                Assert.IsTrue(received.WaitOne(5000), "Content was not received on subscription");
+            }
+        }
+
+        IrisServer _theServer;
+        IrisClientConfig _theClientConfig;
+
+		[SetUp]
+        public void Initialize()
+        {
+            _theServer = new IrisServer();
+            _theServer.Start(56666);
+            _theClientConfig = new IrisClientConfig() { Hostname = "127.0.0.1", Port = 56666 };
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _theServer.Stop();
+        }
+    }
+}
